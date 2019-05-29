57443e10cc53980313fb1a3ad328448eb8f69d8c
Deva <dkothand@gmail.com>
2019-05-29 16:55:34 -0400

    set props in Input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,8 +4,13 @@ class Input extends React.Component {
     render() {
         return(
             <>
-                <label></label>
-                <input/>
+                <label htmlFor={this.props.name}>{this.props.name}</label>
+                <input
+                    id={this.props.name}
+                    name={this.props.name}
+                    type={this.props.type}
+                    value={this.props.value}
+                    placeholder={this.props.placeholder}/>
             </>
         );
     }
