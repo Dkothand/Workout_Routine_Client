f991c1e10eeb7c9387625bc159c0c61e99368050
Deva <dkothand@gmail.com>
2019-05-30 18:14:09 -0400

    linked skeleton CSS and created main.css for styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import RoutineList from './components/RoutineList.js'
+import './css/main.css'
 // import ExerciseList from './components/ExerciseList.js'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <h1>Hello World</h1>
         <RoutineList/>
       </div>
