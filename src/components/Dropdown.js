1c04f9c01731f6f1d71146b61dd81ec7528b9c9d
Deva <dkothand@gmail.com>
2019-05-30 14:42:53 -0400

    Dropdown button shows list of exercises with add button

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -10,12 +10,29 @@ class Dropdown extends React.Component {
     componentDidMount() {
         fetch('/exercises')
         .then(res => res.json())
-        .then(jsonData => console.log(jsonData))
+        .then(jsonData => this.setState({
+            exercises: jsonData
+        }))
         .catch(err => console.error(err))
     }
     render() {
         return(
-            <h5>I'm a Dropdown</h5>
+            <>
+                <h5>Exercises</h5>
+                <ol>
+                	{this.state.exercises.map(exercise => {
+                	    return (
+                            <>
+                            <li 
+                            key={exercise.id}>
+                                {exercise.name}
+                            <button>Add</button>
+                            </li>
+                            </>
+                	    )
+                	})}
+                </ol>
+            </>
         )
     }
 }
